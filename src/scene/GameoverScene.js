ef2447d26d06fe39d878a8281728f7f2b0164744
minimo <minimo.mobile@gmail.com>
2014-06-23 16:31:34 +0900

    game over scene

--- src/scene/GameoverScene.js
+++ src/scene/GameoverScene.js
@@ -16,8 +16,26 @@ tm.define("shotgun.GameoverScene", {
         //バックグラウンド
         this.bg = tm.display.Sprite("greenback", SC_W*2, SC_H*2).addChildTo(this);
 
-        var lb = this.title1 = tm.display.OutlineLabel("SCORE: "+app.lastScore, 50).addChildTo(this);
+        //スコア表示
+        var lb = this.score = tm.display.OutlineLabel("SCORE: "+app.lastScore, 50).addChildTo(this);
         lb.setPosition(SC_W*0.5, SC_H*0.4);
+        lb.fontFamily = "'azuki'";
+        lb.align     = "center";
+        lb.baseline  = "middle";
+        lb.outlineWidth = 2;
+
+        //リトライボタン
+        var sh = this.retry = tm.display.RoundRectangleShape(120, 50).addChildTo(this);
+        sh.setPosition(SC_W*0.25, SC_H*0.6);
+        var lb = this.retryLabel = tm.display.OutlineLabel("RETRY", 50).addChildTo(this);
+        lb.setPosition(SC_W*0.25, SC_H*0.6);
+        lb.fontFamily = "'azuki'";
+        lb.align     = "center";
+        lb.baseline  = "middle";
+        lb.outlineWidth = 2;
+
+        var lb = this.exitLabel = tm.display.OutlineLabel("EXIT", 50).addChildTo(this);
+        lb.setPosition(SC_W*0.75, SC_H*0.6);
         lb.fontFamily = "'azuki'";
         lb.align     = "center";
         lb.baseline  = "middle";
