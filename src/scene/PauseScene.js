13fea8755c4b40c471f0a82347add9a9bf27d723
minimo <minimo.mobile@gmail.com>
2014-06-24 11:39:20 +0900

    pause scene

---
+++ src/scene/PauseScene.js
@@ -0,0 +1,45 @@
+/*
+ *  PauseScene.js
+ *  2014/06/24
+ *  @auther minimo  
+ *  This Program is MIT license.
+ *
+ */
+
+tm.define("shotgun.PauseScene", {
+    superClass: tm.app.Scene,
+
+    init: function() {
+        this.superInit();
+        this.background = "rgba(0, 0, 0, 0.0)";
+
+        //バックグラウンド
+        this.bg = tm.display.Sprite("greenback", SC_W*2, SC_H*2).addChildTo(this);
+
+        //マスク        
+        this.mask = tm.display.Sprite("blackback", SC_W*2, SC_H*2).addChildTo(this);
+        this.mask.tweener.clear().fadeOut(100);
+        
+        this.time = 0;
+    },
+
+    update: function() {
+        this.time++;
+    },
+
+    //タッチorクリック開始処理
+    ontouchstart: function(e) {
+    },
+
+    //タッチorクリック移動処理
+    ontouchmove: function(e) {
+    },
+
+    //タッチorクリック終了処理
+    ontouchend: function(e) {
+//        if (this.time > 30) app.replaceScene(shotgun.MainScene());
+    },
+
+});
+
+
