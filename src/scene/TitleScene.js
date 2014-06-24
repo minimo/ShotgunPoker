746620d7847d64357f77949e64af3f700d039346
minimo <minimo.mobile@gmail.com>
2014-06-24 13:15:06 +0900

    title scene

--- src/scene/TitleScene.js
+++ src/scene/TitleScene.js
@@ -1,5 +1,5 @@
 /*
- *  TitileScene.js
+ *  TitleScene.js
  *  2014/06/19
  *  @auther minimo  
  *  This Program is MIT license.
@@ -28,21 +28,32 @@ tm.define("shotgun.TitleScene", {
         lb.tweener.wait(500).move(SC_W*0.4, SC_H*0.2, 1500,"easeOutBounce");
 
         var lb = this.title2 = tm.display.OutlineLabel("POKER", 120).addChildTo(this.titleLayer);
-        lb.setPosition(SC_W*0.6, SC_H*0.35-SC_H*0.6);
+        lb.setPosition(SC_W*0.7, SC_H*0.35-SC_H*0.6);
         lb.fontFamily = "'azuki'";
         lb.align     = "center";
         lb.baseline  = "middle";
         lb.outlineWidth = 2;
-        lb.tweener.wait(500).move(SC_W*0.6, SC_H*0.35, 1500,"easeOutBounce");
+        lb.tweener.wait(500).move(SC_W*0.7, SC_H*0.35, 1500,"easeOutBounce");
 
-        var lb = this.startLabel = tm.display.OutlineLabel("TOUCH START", 80).addChildTo(this.titleLayer);
-        lb.setPosition(SC_W*0.5, SC_H*0.8);
-        lb.fontFamily = "'KS-Kohichi-FeltPen'";
+        var that = this;
+        var width = 200, height = 70;
+        var param = {fillStyle:'rgba(0,80,0,1)', lineWidth:4};
+
+        //スタート
+        var sh = this.start = tm.display.RoundRectangleShape(width, height, param).addChildTo(this);
+        sh.setPosition(SC_W*0.5, SC_H*0.5);
+        sh.interactive = true;
+        sh.addEventListener("click", function() {
+            that.mask.tweener.clear().fadeIn(300).call(function(){app.replaceScene(shotgun.MainScene());});
+        });
+        sh.param = {fillStyle:'rgba(255,0,0,1)', lineWidth:4}
+
+        var lb = this.startLabel = tm.display.OutlineLabel("START", 50).addChildTo(this);
+        lb.setPosition(SC_W*0.5, SC_H*0.5);
+        lb.fontFamily = "'azuki'";
         lb.align     = "center";
         lb.baseline  = "middle";
-        lb.outlineWidth = 2;
-        lb.alpha = 0;
-        lb.tweener.fadeIn(500);
+        lb.outlineWidth = 4;
 
         this.mask = tm.display.Sprite("blackback", SC_W*2, SC_H*2).addChildTo(this);
         this.mask.tweener.clear().fadeOut(200);
@@ -64,7 +75,7 @@ tm.define("shotgun.TitleScene", {
 
     //タッチorクリック終了処理
     ontouchend: function(e) {
-        if (this.time > 30) app.replaceScene(shotgun.MainScene());
+//        if (this.time > 30) app.replaceScene(shotgun.MainScene());
     },
 
 });
