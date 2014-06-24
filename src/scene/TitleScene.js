331d05b46daf5061bd44385256b6f9d6076f187f
minimo <minimo.mobile@gmail.com>
2014-06-24 16:57:00 +0900

    title scene

--- src/scene/TitleScene.js
+++ src/scene/TitleScene.js
@@ -36,25 +36,66 @@ tm.define("shotgun.TitleScene", {
         lb.tweener.wait(500).move(SC_W*0.7, SC_H*0.35, 1500,"easeOutBounce");
 
         var that = this;
-        var width = 200, height = 70;
+        var width = 230, height = 70;
         var param = {fillStyle:'rgba(0,80,0,1)', lineWidth:4};
 
-        //スタート
-        var sh = this.start = tm.display.RoundRectangleShape(width, height, param).addChildTo(this);
+        //プレイスタート
+        var sh = this.play = tm.display.RoundRectangleShape(width, height, param).addChildTo(this);
         sh.setPosition(SC_W*0.5, SC_H*0.5);
         sh.interactive = true;
         sh.addEventListener("click", function() {
             that.mask.tweener.clear().fadeIn(300).call(function(){app.replaceScene(shotgun.MainScene());});
         });
-        sh.param = {fillStyle:'rgba(255,0,0,1)', lineWidth:4}
-
-        var lb = this.startLabel = tm.display.OutlineLabel("START", 50).addChildTo(this);
+        var lb = this.playLabel = tm.display.OutlineLabel("PLAY", 50).addChildTo(this);
         lb.setPosition(SC_W*0.5, SC_H*0.5);
         lb.fontFamily = "'azuki'";
         lb.align     = "center";
         lb.baseline  = "middle";
         lb.outlineWidth = 4;
 
+        //チュートリアル
+        var sh = this.tutorial = tm.display.RoundRectangleShape(width, height, param).addChildTo(this);
+        sh.setPosition(SC_W*0.5, SC_H*0.6);
+        sh.interactive = true;
+        sh.addEventListener("click", function() {
+            that.mask.tweener.clear().fadeIn(300).call(function(){app.replaceScene(shotgun.MainScene());});
+        });
+        var lb = this.tutorialLabel = tm.display.OutlineLabel("TUTORIAL", 50).addChildTo(this);
+        lb.setPosition(SC_W*0.5, SC_H*0.6);
+        lb.fontFamily = "'azuki'";
+        lb.align     = "center";
+        lb.baseline  = "middle";
+        lb.outlineWidth = 4;
+
+        //設定
+        var sh = this.setting = tm.display.RoundRectangleShape(width, height, param).addChildTo(this);
+        sh.setPosition(SC_W*0.5, SC_H*0.7);
+        sh.interactive = true;
+        sh.addEventListener("click", function() {
+            that.mask.tweener.clear().fadeIn(300).call(function(){app.replaceScene(shotgun.MainScene());});
+        });
+        var lb = this.settingLabel = tm.display.OutlineLabel("SETTING", 50).addChildTo(this);
+        lb.setPosition(SC_W*0.5, SC_H*0.7);
+        lb.fontFamily = "'azuki'";
+        lb.align     = "center";
+        lb.baseline  = "middle";
+        lb.outlineWidth = 4;
+
+        //クレジット
+        var sh = this.credit = tm.display.RoundRectangleShape(width, height, param).addChildTo(this);
+        sh.setPosition(SC_W*0.5, SC_H*0.8);
+        sh.interactive = true;
+        sh.addEventListener("click", function() {
+            that.mask.tweener.clear().fadeIn(300).call(function(){app.replaceScene(shotgun.CreditScene());});
+        });
+        var lb = this.creditLabel = tm.display.OutlineLabel("CREDIT", 50).addChildTo(this);
+        lb.setPosition(SC_W*0.5, SC_H*0.8);
+        lb.fontFamily = "'azuki'";
+        lb.align     = "center";
+        lb.baseline  = "middle";
+        lb.outlineWidth = 4;
+
+        //マスク
         this.mask = tm.display.Sprite("blackback", SC_W*2, SC_H*2).addChildTo(this);
         this.mask.tweener.clear().fadeOut(200);
         
