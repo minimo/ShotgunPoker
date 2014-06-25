4a9dd1ccc34be5ae54b82dcd101829b7276f859c
minimo <minimo.mobile@gmail.com>
2014-06-25 14:15:19 +0900

    setting scene

---
+++ src/scene/SettingScene.js
@@ -0,0 +1,42 @@
+/*
+ *  SettingScene.js
+ *  2014/06/25
+ *  @auther minimo  
+ *  This Program is MIT license.
+ *
+ */
+
+tm.define("shotgun.SettingScene", {
+    superClass: tm.app.Scene,
+
+    init: function() {
+        this.superInit();
+        this.background = "rgba(0, 0, 0, 0.0)";
+
+        //バックグラウンド
+        this.bg = tm.display.Sprite("greenback", SC_W*2, SC_H*2).addChildTo(this);
+        
+        this.mask = tm.display.Sprite("blackback", SC_W*2, SC_H*2).addChildTo(this);
+        this.mask.tweener.clear().fadeOut(1000);
+        
+        this.time = 0;
+    },
+
+    update: function() {
+        this.time++;
+    },
+
+    //タッチorクリック開始処理
+    ontouchstart: function(e) {
+    },
+
+    //タッチorクリック移動処理
+    ontouchmove: function(e) {
+    },
+
+    //タッチorクリック終了処理
+    ontouchend: function(e) {
+    },
+});
+
+
