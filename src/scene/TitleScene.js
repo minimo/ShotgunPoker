d0e262d65723a7194cf8c7d69f26413df218e29c
minimo <minimo.mobile@gmail.com>
2014-07-08 16:21:00 +0900

    tutorial

--- src/scene/TitleScene.js
+++ src/scene/TitleScene.js
@@ -12,14 +12,33 @@ tm.define("shotgun.TitleScene", {
     init: function() {
         this.superInit();
         this.background = "rgba(0, 0, 0, 0.0)";
+//        this.checkHierarchy = true;
 
         //バックグラウンド
         this.bg = tm.display.Sprite("greenback", SC_W, SC_H).addChildTo(this);
         this.bg.setPosition(SC_W/2, SC_H/2);
-        
+
         this.titleLayer = tm.app.Object2D().addChildTo(this);
+        this.titleLayer.checkHierarchy = true;
         this.tutorialLayer = tm.app.Object2D().addChildTo(this);
 
+        this.setupTitle();
+        this.setupTutorial1();
+
+        //マスク
+        this.mask = tm.display.Sprite("blackback", SC_W*2, SC_H*2).addChildTo(this);
+        this.mask.tweener.clear().fadeOut(200);
+        
+        this.time = 0;
+
+        appMain.playBGM("titleBGM");
+    },
+
+    onenter: function() {
+        this.mask.tweener.clear().fadeOut(200);
+    },
+
+    setupTitle: function() {
         var lb = this.title1 = tm.display.OutlineLabel("SHOTGUN", 120).addChildTo(this.titleLayer);
         lb.fontFamily = "'azuki'"; lb.align = "center"; lb.baseline = "middle"; lb.outlineWidth = 2;
         lb.setPosition(SC_W*0.4, SC_H*0.2);
@@ -33,64 +52,92 @@ tm.define("shotgun.TitleScene", {
         var param = {fillStyle:'rgba(0,80,0,1)', lineWidth:4};
 
         //プレイスタート
-        var sh = this.play = tm.display.RoundRectangleShape(width, height, param).addChildTo(this);
+        var sh = this.play = tm.display.RoundRectangleShape(width, height, param).addChildTo(this.titleLayer);
         sh.setPosition(SC_W*0.5, SC_H*0.5);
         sh.interactive = true;
+        sh.checkHierarchy = true;
         sh.boundingType = "rect";
-        sh.addEventListener("click", function() {
+        sh.addEventListener("pointingend", function() {
             that.mask.tweener.clear().fadeIn(200).call(function(){appMain.replaceScene(shotgun.MainScene());});
         });
-        var lb = this.playLabel = tm.display.OutlineLabel("PLAY", 50).addChildTo(this);
+        var lb = this.playLabel = tm.display.OutlineLabel("PLAY", 50).addChildTo(this.titleLayer);
         lb.fontFamily = "'azuki'"; lb.align = "center"; lb.baseline = "middle"; lb.outlineWidth = 4;
         lb.setPosition(SC_W*0.5, SC_H*0.5);
 
         //チュートリアル
-        var sh = this.tutorial = tm.display.RoundRectangleShape(width, height, param).addChildTo(this);
+        var sh = this.tutorial = tm.display.RoundRectangleShape(width, height, param).addChildTo(this.titleLayer);
         sh.setPosition(SC_W*0.5, SC_H*0.6);
         sh.interactive = true;
+        sh.checkHierarchy = true;
         sh.boundingType = "rect";
-        sh.addEventListener("click", function() {
-            that.mask.tweener.clear().fadeIn(200).call(function(){appMain.pushScene(shotgun.MainScene());});
+        sh.addEventListener("pointingend", function() {
+            that.titleLayer.tweener.clear().moveBy(-SC_W, 0, 1000, "easeOutQuint");
         });
-        var lb = this.tutorialLabel = tm.display.OutlineLabel("TUTORIAL", 50).addChildTo(this);
+        var lb = this.tutorialLabel = tm.display.OutlineLabel("TUTORIAL", 50).addChildTo(this.titleLayer);
         lb.fontFamily = "'azuki'"; lb.align = "center"; lb.baseline = "middle"; lb.outlineWidth = 4;
         lb.setPosition(SC_W*0.5, SC_H*0.6);
 
         //設定
-        var sh = this.setting = tm.display.RoundRectangleShape(width, height, param).addChildTo(this);
+        var sh = this.setting = tm.display.RoundRectangleShape(width, height, param).addChildTo(this.titleLayer);
         sh.setPosition(SC_W*0.5, SC_H*0.7);
         sh.interactive = true;
+        sh.checkHierarchy = true;
         sh.boundingType = "rect";
-        sh.addEventListener("click", function() {
+        sh.addEventListener("pointingend", function() {
             that.mask.tweener.clear().fadeIn(200).call(function(){appMain.pushScene(shotgun.SettingScene());});
         });
-        var lb = this.settingLabel = tm.display.OutlineLabel("SETTING", 50).addChildTo(this);
+        var lb = this.settingLabel = tm.display.OutlineLabel("SETTING", 50).addChildTo(this.titleLayer);
         lb.fontFamily = "'azuki'"; lb.align = "center"; lb.baseline = "middle"; lb.outlineWidth = 4;
         lb.setPosition(SC_W*0.5, SC_H*0.7);
 
         //クレジット
-        var sh = this.credit = tm.display.RoundRectangleShape(width, height, param).addChildTo(this);
+        var sh = this.credit = tm.display.RoundRectangleShape(width, height, param).addChildTo(this.titleLayer);
         sh.setPosition(SC_W*0.5, SC_H*0.8);
         sh.interactive = true;
+        sh.checkHierarchy = true;
         sh.boundingType = "rect";
-        sh.addEventListener("click", function() {
+        sh.addEventListener("pointingend", function() {
             that.mask.tweener.clear().fadeIn(200).call(function(){appMain.pushScene(shotgun.CreditScene());});
         });
-        var lb = this.creditLabel = tm.display.OutlineLabel("CREDIT", 50).addChildTo(this);
+        var lb = this.creditLabel = tm.display.OutlineLabel("CREDIT", 50).addChildTo(this.titleLayer);
         lb.fontFamily = "'azuki'"; lb.align = "center"; lb.baseline = "middle"; lb.outlineWidth = 4;
         lb.setPosition(SC_W*0.5, SC_H*0.8);
-
-        //マスク
-        this.mask = tm.display.Sprite("blackback", SC_W*2, SC_H*2).addChildTo(this);
-        this.mask.tweener.clear().fadeOut(200);
-        
-        this.time = 0;
-
-        appMain.playBGM("titleBGM");
     },
 
-    onenter: function() {
-        this.mask.tweener.clear().fadeOut(200);
+    setupTutorial1: function() {
+        var that = this;
+        var width = 230, height = 60;
+        var param = {fillStyle:'rgba(0,80,0,1)', lineWidth:4};
+
+        var lb = this.credit1 = tm.display.OutlineLabel("TUTORIAL", 40).addChildTo(this.titleLayer);
+        lb.fontFamily = "'azuki'"; lb.align = "center"; lb.baseline = "middle"; lb.outlineWidth = 2;
+        lb.setPosition(SC_W*1.5, SC_H*0.05);
+
+        //戻る
+        var sh = tm.display.RoundRectangleShape(width, height, param).addChildTo(this.titleLayer);
+        sh.setPosition(SC_W*1.25, SC_H*0.9);
+        sh.interactive = true;
+        sh.checkHierarchy = true;
+        sh.boundingType = "rect";
+        sh.addEventListener("pointingend", function() {
+            that.titleLayer.tweener.clear().moveBy(SC_W, 0, 1000, "easeOutQuint");
+        });
+        var lb = tm.display.OutlineLabel("PREV", 50).addChildTo(this.titleLayer);
+        lb.fontFamily = "'azuki'"; lb.align = "center"; lb.baseline = "middle"; lb.outlineWidth = 4;
+        lb.setPosition(SC_W*1.25, SC_H*0.9);
+
+        //次
+        var sh = tm.display.RoundRectangleShape(width, height, param).addChildTo(this.titleLayer);
+        sh.setPosition(SC_W*1.75, SC_H*0.9);
+        sh.interactive = true;
+        sh.checkHierarchy = true;
+        sh.boundingType = "rect";
+        sh.addEventListener("pointingend", function() {
+            that.titleLayer.tweener.clear().moveBy(-SC_W, 0, 1000, "easeOutQuint");
+        });
+        var lb = tm.display.OutlineLabel("NEXT", 50).addChildTo(this.titleLayer);
+        lb.fontFamily = "'azuki'"; lb.align = "center"; lb.baseline = "middle"; lb.outlineWidth = 4;
+        lb.setPosition(SC_W*1.75, SC_H*0.9);
     },
 
     update: function() {
