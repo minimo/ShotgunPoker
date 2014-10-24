053320d36432de3d09c12315e00f56fca4906dfa
minimo <minimo.mobile@gmail.com>
2014-10-24 18:27:25 +0900

    ボタンをボタンっぽくした

---
+++ src/Button.js
@@ -0,0 +1,55 @@
+/*
+ *  Button.js
+ *  2014/06/24
+ *  @auther minimo  
+ *  This Program is MIT license.
+ *
+ */
+
+tm.define("shotgun.Button", {
+    superClass: tm.display.RoundRectangleShape,
+
+    //ラベル用フォントパラメータ
+    labelParam: {fontFamily:"'azuki'", align: "center", baseline:"middle", outlineWidth:3 },
+
+    push: false,
+    down: 5,
+
+    init: function(width, height, text) {
+        this.superInit(width, height, {fillStyle:'rgba(0,80,0,1)', lineWidth:4});
+
+        this.interactive = true;
+        this.boundingType = "rect";
+        this.addEventListener("pointingstart", function() {
+            this.push = true;
+            this.y += this.down;
+        });
+        this.addEventListener("pointingmove", function(e) {
+            var pt = e.pointing;
+            if (this.isHitPoint(pt.x, pt.y)) {
+                if (!this.push) {
+                    this.push = true;
+                    this.y += this.down;
+                }
+            } else {
+                if (this.push) {
+                    this.push = false;
+                    this.y -= this.down;
+                }
+            }
+        });
+        this.addEventListener("pointingend", function(e) {
+            var pt = e.pointing;
+            if (this.isHitPoint(pt.x, pt.y)) {
+                var e = tm.event.Event("pushed");
+                this.dispatchEvent(e);
+                this.push = false;
+                this.y -= this.down;
+            }
+        });
+
+        tm.display.OutlineLabel(text, 50)
+            .addChildTo(this)
+            .setParam(this.labelParam);
+    },
+});
