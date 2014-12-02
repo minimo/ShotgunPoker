611cd2162c95e9a90f978fb6e9596e477caf755f
minimo <minimo.mobile@gmail.com>
2014-12-02 17:39:28 +0900

    サウンド管理クラス暫定

--- src/SoundSet.js
+++ src/SoundSet.js
@@ -48,6 +48,8 @@ tm.define("shotgun.SoundSet", {
         var media = this.find(name);
         if (media) {
             media.play(true);
+            media.setVolume(this.volumeBGM);
+            this.bgm = media;
         }
         return this;
     },
@@ -55,6 +57,7 @@ tm.define("shotgun.SoundSet", {
     stopBGM: function() {
         if (this.bgm) {
             this.bgm.stop();
+            this.bgm = null;
         }
         return this;
     },
@@ -68,18 +71,21 @@ tm.define("shotgun.SoundSet", {
     },
 
     setVolumeBGM: function(v) {
+        this.volumeSE = Math.Clamp(v, 0, 1);
         return this;
     },
 
     playSE: function(name) {
         var media = this.find(name);
         if (media) {
-            media.play(false);
+            media.setVolume(this.volumeSE);
+            media.playClone();
         }
         return this;
     },
 
     setVolumeSE: function(v) {
+        this.volumeSE = Math.Clamp(v, 0, 1);
         return this;
     },
 });
@@ -87,11 +93,14 @@ tm.define("shotgun.SoundSet", {
 tm.define("shotgun.SoundElement", {
     type: 0,
     name: null,
+    url: null,
     media: null,
+    volume: 1.0,
     status: null,
     init: function(type, name, url) {
         this.type = type;
         this.name = name;
+        this.url = url;
 
         if (type == MEDIA_ASSET) {
             this.media = tm.asset.AssetManager.get(name);
@@ -101,6 +110,7 @@ tm.define("shotgun.SoundElement", {
         }
     },
 
+    //再生
     play: function(loop) {
         if (!this.media) return this;
         if (this.type == MEDIA_URL) {
@@ -116,8 +126,31 @@ tm.define("shotgun.SoundElement", {
         return this;
     },
 
+    //クローンを作って再生（リピート無し）
+    playClone: function() {
+        if (!this.media) return this;
+        if (this.type == MEDIA_URL) {
+            var tmp = new Media(getPath()+this.url);
+            tmp.play();
+            tmp = null;
+        } else {
+            this.media.loop = false;
+            this.media.clone().play();
+        }
+        return this;
+    },
+
     stop: function() {
         if (!this.media) return this;
         this.media.stop();
+        return this;
+    },
+
+    setVolume: function(vol) {
+        if (!this.media) return this;
+        vol = vol || 1.0;
+        this.volume = Math.clamp(vol, 0.0, 1.0);
+        this.media.volume = this.volume;
+        return this;
     },
 });
