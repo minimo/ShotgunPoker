797a8febf023bac05b656e7cc9292399a9038be4
minimo <minimo.mobile@gmail.com>
2015-01-03 15:44:59 +0900

    LoadingScene更新

--- src/scene/LoadingScene.js
+++ src/scene/LoadingScene.js
@@ -7,9 +7,11 @@
  */
 
 tm.define("shotgun.LoadingScene", {
-    superClass: "tm.ui.LoadingScene",
+    superClass: "tm.app.Scene",
 
-    init: function() {
+    init: function(param) {
+        this.superInit();
+
         var param = {
             assets: assets,
             width: SC_W,
@@ -20,9 +22,100 @@ tm.define("shotgun.LoadingScene", {
                 return shotgun.TitleScene();
             }.bind(this),
         };
-        this.superInit(param);
-    },
 
+        this.fromJSON({
+            children: {
+                stage: {
+                    type: "tm.display.CanvasElement",
+                },
+            }
+        });
+
+        this.stage.fromJSON({
+            children: {
+                bg: {
+                    type: "tm.display.Shape",
+                    init: [param.width, param.height],
+                    originX: 0,
+                    originY: 0,
+                },
+                piyoLayer: {
+                    type: "tm.display.CanvasElement",
+                },
+                label: {
+                    type: "tm.display.Label",
+                    text: "LOADING",
+                    x: param.width/2,
+                    y: param.height/2-20,
+                    align: "center",
+                    baseline: "middle",
+                    fontSize: 46,
+                    shadowBlur: 4,
+                    shadowColor: "hsl(190, 100%, 50%)",
+                },
+                bar: {
+                    type: "tm.ui.Gauge",
+                    init: [{
+                        width: param.width,
+                        height: 10,
+                        color: "hsl(200, 100%, 80%)",
+                        bgColor: "transparent",
+                        borderColor: "transparent",
+                        borderWidth: 0,
+                    }],
+                    x: 0,
+                    y: 0,
+                },
+            }
+        });
+
+        // bg
+        var bg = this.stage.bg;
+        bg.canvas.clearColor(param.bgColor);
+
+        // label
+        var label = this.stage.label;
+        label.tweener
+            .to({alpha:1}, 1000)
+            .to({alpha:0.5}, 1000)
+            .setLoop(true)
+
+        // bar
+        var bar = this.stage.bar;
+        bar.animationFlag = false;
+        bar.value = 0;
+        bar.animationFlag = true;
+        bar.animationTime = 100;
+
+        var stage = this.stage;
+        stage.alpha = 0.0;
+        stage.tweener.clear().fadeIn(100).call(function() {
+            if (param.assets) {
+                var loader = tm.asset.Loader();
+                loader.onload = function() {
+                    stage.tweener.clear().wait(200).fadeOut(200).call(function() {
+                        if (param.nextScene) {
+                                this.app.replaceScene(param.nextScene());
+                        }
+                        var e = tm.event.Event("load");
+                        this.fire(e);
+                    }.bind(this));
+                }.bind(this);
+
+                loader.onprogress = function(e) {
+                    // update bar
+                    bar.value = e.progress*100;
+
+                    // dispatch event
+                    var event = tm.event.Event("progress");
+                    event.progress = e.progress;
+                    this.fire(event);
+                }.bind(this);
+
+                loader.load(param.assets);
+            }
+        }.bind(this));
+    },
     _onLoadAssets: function() {
         appMain.sounds.add("titleBGM",    "assets/game_maoudamashii_5_casino02.mp3");
         appMain.sounds.add("mainBGM",     "assets/game_maoudamashii_5_casino01.mp3");
