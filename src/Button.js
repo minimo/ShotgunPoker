251ea19434c5d46c478f71e8c2c706222dd54f5d
minimo <minimo.mobile@gmail.com>
2015-01-07 15:55:41 +0900

    角丸ボタン追加

--- src/Button.js
+++ src/Button.js
@@ -18,6 +18,7 @@ tm.define("shotgun.Button", {
         fontFamily: "azuki",
         fontSize: 50,
     },
+
     labelParam: {fontFamily: "azuki", align: "center", baseline:"middle", outlineWidth:3 },
 
     text: "",
@@ -100,15 +101,149 @@ tm.define("shotgun.Button", {
         var width = this.width, height = this.height;
 
         //ボタン影
-        this.shadow = tm.display.RectangleShape(width, height, {fillStyle: style.shadowColor, strokeStyle: style.shadowColor, lineWidth: style.lineWidth})
+        var shadowStyle = {
+            fillStyle: style.shadowColor,
+            strokeStyle: style.shadowColor,
+            lineWidth: style.lineWidth
+        };
+        this.shadow = tm.display.RectangleShape(width, height, shadowStyle)
             .addChildTo(this)
             .setPosition(this.downX, this.downY);
         this.shadow.blendMode = "source-over";
 
         //ボタン本体
-        this.button = tm.display.RectangleShape(width, height, {fillStyle: style.buttonColor, strokeStyle: style.lineColor, lineWidth:  style.lineWidth})
+        var buttonStyle = {
+            fillStyle: style.buttonColor,
+            strokeStyle: style.lineColor,
+            lineWidth: style.lineWidth
+        };
+        this.button = tm.display.RectangleShape(width, height, buttonStyle)
             .addChildTo(this);
-//        this.button.blendMode = "lighter";
+
+        //ボタンラベル
+        this.labelParam.fontFamily = style.fontFamily;
+        this.label = tm.display.OutlineLabel(this.text, style.fontSize)
+            .addChildTo(this.button)
+            .setParam(this.labelParam);
+    },
+});
+
+tm.define("shotgun.RoundButton", {
+    superClass: tm.app.Object2D,
+
+    //描画スタイル設定
+    DEFAULT_STYLE: {
+        buttonColor: 'rgba(50, 150, 255, 0.8)',
+        lineColor: 'rgba(200, 200, 200, 0.5)',
+        lineWidth: 4,
+        shadowColor: 'rgba(0, 0, 0, 0.5)',
+        fontFamily: "azuki",
+        fontSize: 50,
+        radius: 16,
+    },
+
+    labelParam: {fontFamily: "azuki", align: "center", baseline:"middle", outlineWidth:3 },
+
+    text: "",
+    push: false,
+    lock: false,
+
+    //ボタン押下時の移動量
+    downX: 0,
+    downY: 10,
+
+    init: function(width, height, text, style) {
+        this.superInit();
+
+        this.width = width || 200;
+        this.height = height || 80;
+        this.text = text || "";
+
+        //セットアップ
+        this.setup(style);
+
+        //判定処理設定
+        this.interactive = true;
+        this.boundingType = "rect";
+//        this.checkHierarchy = true;
+
+        //イベントリスナ登録
+        this.addEventListener("touchstart", function() {
+            if (this.lock) return;
+
+            this.push = true;
+            this.button.x += this.downX;
+            this.button.y += this.downY;
+            var e = tm.event.Event("push");
+            this.dispatchEvent(e);
+        });
+        this.addEventListener("touchmove", function(e) {
+            if (this.lock) return;
+
+            var pt = e.pointing;
+            if (this.isHitPoint(pt.x, pt.y)) {
+                if (!this.push) {
+                    this.push = true;
+                    this.button.x += this.downX;
+                    this.button.y += this.downY;
+                }
+            } else {
+                if (this.push) {
+                    this.push = false;
+                    this.button.x -= this.downX;
+                    this.button.y -= this.downY;
+                }
+            }
+        });
+        this.addEventListener("touchend", function(e) {
+            if (this.lock) return;
+
+            var pt = e.pointing;
+            if (this.isHitPoint(pt.x, pt.y)) {
+                this.push = false;
+                this.button.x -= this.downX;
+                this.button.y -= this.downY;
+
+                var e = tm.event.Event("pushed");
+                this.dispatchEvent(e);
+            }
+        });
+    },
+
+    setup: function(style) {
+        style = style || {};
+        style.$safe(this.DEFAULT_STYLE)
+
+        //登録済みの場合破棄する
+        if (this.shadow) {
+            this.shadow.remove();
+            this.label.remove();
+            this.button.remove();
+        }
+
+        var width = this.width, height = this.height;
+
+        //ボタン影
+        var shadowStyle = {
+            fillStyle: style.shadowColor,
+            strokeStyle: style.shadowColor,
+            lineWidth: style.lineWidth,
+            radius: style.radius,
+        };
+        this.shadow = tm.display.RoundRectangleShape(width, height, shadowStyle)
+            .addChildTo(this)
+            .setPosition(this.downX, this.downY);
+        this.shadow.blendMode = "source-over";
+
+        //ボタン本体
+        var buttonStyle = {
+            fillStyle: style.buttonColor,
+            strokeStyle: style.lineColor,
+            lineWidth: style.lineWidth,
+            radius: style.radius,
+        };
+        this.button = tm.display.RoundRectangleShape(width, height, buttonStyle)
+            .addChildTo(this);
 
         //ボタンラベル
         this.labelParam.fontFamily = style.fontFamily;
@@ -241,15 +376,24 @@ tm.define("shotgun.ToggleButton", {
         var width = this.width, height = this.height;
 
         //ボタン影
-        this.shadow = tm.display.RectangleShape(width, height, {fillStyle: style.shadowColor, strokeStyle: style.shadowColor, lineWidth: style.lineWidth})
+        var shadowStyle = {
+            fillStyle: style.shadowColor,
+            strokeStyle: style.shadowColor,
+            lineWidth: style.lineWidth
+        };
+        this.shadow = tm.display.RectangleShape(width, height, shadowStyle)
             .addChildTo(this)
             .setPosition(this.downX, this.downY);
         this.shadow.blendMode = "source-over";
 
         //ボタン本体
-        this.button = tm.display.RectangleShape(width, height, {fillStyle: style.buttonColor, strokeStyle: style.lineColor, lineWidth:  style.lineWidth})
+        var buttonStyle = {
+            fillStyle: style.buttonColor,
+            strokeStyle: style.lineColor,
+            lineWidth: style.lineWidth
+        };
+        this.button = tm.display.RectangleShape(width, height, buttonStyle)
             .addChildTo(this);
-//        this.button.blendMode = "lighter";
 
         //ボタンラベル
         this.labelParam.fontFamily = style.fontFamily;
