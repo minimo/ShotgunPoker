f6f4f0fb3d4d239ea3d1ea25c25c102191d37aba
minimo <minimo.mobile@gmail.com>
2015-02-09 15:43:14 +0900

    実績保存とか

--- src/Achievement.js
+++ src/Achievement.js
@@ -27,10 +27,21 @@ tm.define("shotgun.Achievement", {
 
     //ローカルストレージへ保存
     save: function() {
+        var saveObj = {};
+        var list = shotgun.achievementList;
+        for (var i = 0; i < list.length; i++) {
+            var obj = list[i];
+            saveObj[obj.id] = {
+                "id": obj.id,
+                "percent": obj.percent,
+            }
+        }
+        localStorage.setItem("achievement", JSON.stringify(saveObj));
     },
 
     //ローカルストレージから読み込み
     load: function() {
+        var ac = localStorage.getItem("achievement");
     },
 
     //ゲームセンターから実績情報を読み込み
@@ -39,5 +50,16 @@ tm.define("shotgun.Achievement", {
 
     //ゲームセンターと実績情報同期
     syncGamecenter: function() {
+    },
+
+    //全実績をリセット
+    reset: function() {
+        var acList = [];
+        var ac = shotgun.achievementList;
+        var len = shotgun.achievementList.length;
+        for (var i = 0; i < len; i++) {
+            var obj = ac[i];
+            ac.percent = "0";
+        }
     },
 });
