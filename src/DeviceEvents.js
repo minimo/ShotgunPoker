3c2217c738a4ce3aa7f59b328f3446aa1cb4f7f2
minimo <minimo.mobile@gmail.com>
2015-02-16 11:44:59 +0900

    SNS送信機能実装

--- src/DeviceEvents.js
+++ src/DeviceEvents.js
@@ -271,12 +271,54 @@ document.addEventListener('onInterstitialDissmiss', onInterstitialDissmiss, fals
  *
  */
 
-//ソーシャルにメッセージを送る
-var sendSocialMessage = function() {
+//SNSにメッセージを送（ゲームオーバー時）
+var sendSocialMessage = function(mode, returnJoker, score) {
     if (!ENABLE_SOCIAL) return false;
 
+    //メッセージテキストの作成
+    var lb = "Normal";
+    if (mode == GAMEMODE_HARD) lb = "Hard";
+    if (returnJoker) lb += "-RJ";
+
+    var url = "https://itunes.apple.com/jp/app/shottoganpoka/id951249463";
+    var text = "Score:"+score+"pts.("+lb+")【"+url+"】 #ShotgunPoker";
     var message = {
-        text: "This is a test message",
+        text: text,
+//        activityTypes: ["PostToTwitter"],
+    };
+    window.socialmessage.send(message);
+}
+
+//SNSにメッセージを送る（総合情報）
+var sendSocialMessageGlobal = function() {
+    if (!ENABLE_SOCIAL) return false;
+
+    var score = 0;
+    var mode = GAMEMODE_NORMAL;
+    var returnJoker = false;
+
+    //ベストスコアはどれか
+    var sc1 = appMain.highScore[GAMEMODE_NORMAL];
+    var sc2 = appMain.highScore[GAMEMODE_NORMAL+10];
+    var sc3 = appMain.highScore[GAMEMODE_HARD];
+    var sc4 = appMain.highScore[GAMEMODE_HARD+10];
+    var sc = [sc1, sc2, sc3, sc4];
+    sc.sort(function(a, b){return b-a;});
+    score = sc[0];
+    if (sc1 == sc[0]) {mode = GAMEMODE_NORMAL; returnJoker = false;}
+    if (sc2 == sc[0]) {mode = GAMEMODE_NORMAL; returnJoker = true;}
+    if (sc3 == sc[0]) {mode = GAMEMODE_HARD;   returnJoker = false;}
+    if (sc4 == sc[0]) {mode = GAMEMODE_HARD;   returnJoker = true;}
+
+    //メッセージテキストの作成
+    var lb = "Normal";
+    if (mode == GAMEMODE_HARD) lb = "Hard";
+    if (returnJoker) lb += "-RJ";
+
+    var url = "https://itunes.apple.com/jp/app/shottoganpoka/id951249463";
+    var text = "MyBestScore:"+score+"pts.("+lb+")【"+url+"】 #ShotgunPoker";
+    var message = {
+        text: text,
 //        activityTypes: ["PostToTwitter"],
     };
     window.socialmessage.send(message);
