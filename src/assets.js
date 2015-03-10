ccb9803c94ca70b11597b4783d96ebaff79daa07
minimo <minimo.mobile@gmail.com>
2015-03-10 09:50:41 +0900

    アセット読み込み順序変更

--- src/assets.js
+++ src/assets.js
@@ -6,11 +6,26 @@
  */
 
 var assets = {
+    //Font
+    "Yasashisa":    "font/07YasashisaAntique.ttf",
+    "CasinoRegular":"font/CasinoRegular.ttf",
+    "UbuntuMono":   "font/UbuntuMono-Bold.ttf",
+
     //BGM
     "titleBGM":     "assets/game_maoudamashii_5_casino02.mp3",
     "mainBGM":      "assets/game_maoudamashii_5_casino01.mp3",
     "hardBGM":      "assets/game_maoudamashii_5_casino03.mp3",
     "tutorialBGM":  "assets/game_maoudamashii_5_casino04.mp3",
+
+    //SE
+    "countdown":    "assets/se_countdown.mp3",
+    "deal":         "assets/se_deal.mp3",
+    "dist":         "assets/se_maoudamashii_se_paper01.mp3",
+    "hand":         "assets/se_hand.mp3",
+    "nopair":       "assets/se_nopair.mp3",
+    "extend":       "assets/ata_a17.mp3",
+    "achievement":  "assets/ata_a49.mp3",
+    "button":       "assets/botan_b25.mp3",
 
     //images
     "card":         "assets/trump.png",
@@ -24,18 +39,4 @@ var assets = {
     "arrow":        "assets/arrow.png",
     "finger":       "assets/finger.png",
 
-    //Font
-    "Yasashisa":    "font/07YasashisaAntique.ttf",
-    "CasinoRegular":"font/CasinoRegular.ttf",
-    "UbuntuMono":   "font/UbuntuMono-Bold.ttf",
-
-    //SE
-    "countdown":    "assets/se_countdown.mp3",
-    "deal":         "assets/se_deal.mp3",
-    "dist":         "assets/se_maoudamashii_se_paper01.mp3",
-    "hand":         "assets/se_hand.mp3",
-    "nopair":       "assets/se_nopair.mp3",
-    "extend":       "assets/ata_a17.mp3",
-    "achievement":  "assets/ata_a49.mp3",
-    "button":       "assets/botan_b25.mp3",
 }
